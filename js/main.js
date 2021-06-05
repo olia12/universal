c89f6d3eb05fb05b4641626eed1d0845b9536b78
olia12 <686371olia@mail.ru>
2021-06-05 23:53:28 +0300

    create: mob menu fix: tabs

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  var tabsItem = $(".main__tab");
+  var contentItem = $(".main__element");
+
+  tabsItem.on("click", function (event) {
+    var activeContent = $(this).attr("data-target");
+    tabsItem.removeClass("main__tab--active");
+    contentItem.removeClass("main__element--active");
+    $(activeContent).addClass("main__element--active");
+    $(this).addClass("main__tab--active");
+  });
+});
+
+var menuButton = document.querySelector(".header__button-menu");
+menuButton.addEventListener("click", function () {
+  document
+    .querySelector(".header__background-menu")
+    .classList.toggle("header__background-menu--visible");
+});
