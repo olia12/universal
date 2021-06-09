dae57928748b7f692f9a910545c1dc2678ed8027
olia12 <686371olia@mail.ru>
2021-06-10 01:41:23 +0300

    create: slider for hints section

--- js/main.js
+++ js/main.js
@@ -24,3 +24,15 @@ $(document).on("click", ".jquery path", function () {
 $(document).on("click", ".bookmark-path path", function () {
   $(this)[0].classList.toggle("active");
 });
+
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
